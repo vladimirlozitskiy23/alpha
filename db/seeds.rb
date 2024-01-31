deb7117ce87b42d783ec7ba23e8b1c6fb7f15210
vladimirlozitskiy23 <rubyonrails2023@gmail.com>
2024-01-31 17:02:43 +0300

    Seed db

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+News.create!(title: 'test_news', body: 'test_text')
+News.create!(title: 'test_news_1', body: 'test_text_1')
+News.create!(title: 'test_news_2', body: 'test_text_2')
+News.create!(title: 'test_news_3', body: 'test_text_3')
+News.create!(title: 'test_news_4', body: 'test_text_4')
