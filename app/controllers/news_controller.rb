56cb45bfa6aa1193f15f663dd3928095e00d93ec
vladimirlozitskiy23 <rubyonrails2023@gmail.com>
2024-02-23 17:04:59 +0300

    Add attachment

--- app/controllers/news_controller.rb
+++ app/controllers/news_controller.rb
@@ -3,4 +3,33 @@ class NewsController < ApplicationController
   def index
     @news = News.all
   end
+
+  def new
+    @new = News.new
+  end
+
+  def create 
+    @new = News.new(new_params)
+    if @new.save 
+      redirect_to @new
+    else
+      render :new
+    end
+  end
+
+  def show
+    @new = News.find(params[:id])
+  end
+
+  def destroy
+    @new = News.find(params[:id])
+    @new.destroy
+    redirect_to @new
+  end
+
+  private
+
+  def new_params
+    params.require(:news).permit(:title, :body, :image, pictures:[])
+  end  
 end
--- app/models/news.rb
+++ app/models/news.rb
@@ -1,2 +1,5 @@
 class News < ApplicationRecord
+  has_one_attached :image
+  has_many_attached :pictures
+  has_rich_text :body
 end
