534538bde73ad2f2aef04d11ff94cb2e7e0fe62f
vladimirlozitskiy23 <rubyonrails2023@gmail.com>
2024-10-29 21:09:03 +0300

    Add actions and views stubs

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,2 +1,21 @@
 class PagesController < ApplicationController
+  
+  def history
+  end
+  
+  def contacts
+  end
+
+  def support
+  end
+
+  def payment
+  end
+
+  def award
+  end
+
+  def newspaper
+  end
+
 end
