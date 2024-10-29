2813e2c46f30262bc3db69710be43c029aac6377
vladimirlozitskiy23 <rubyonrails2023@gmail.com>
2024-10-29 21:09:27 +0300

    Add routes for pages

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,12 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
    root "news#index"
    resources :news
+
+   get 'pages/history', to: 'pages#history'
+   get 'pages/contacts', to: 'pages#contacts'
+   get 'pages/support', to: 'pages#support'
+   get 'pages/payment,', to: 'pages#payment,'
+   get 'pages/award,', to: 'pages#award,'
+   get 'pages/newspaper', to: 'pages#newspaper'
+
 end
