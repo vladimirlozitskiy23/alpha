8904d52d4e142fde19649331db4b2b2d3de66829
vladimirlozitskiy23 <rubyonrails2023@gmail.com>
2026-01-26 17:33:08 +0300

    Create club model and db

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2026_01_26_133558) do
+ActiveRecord::Schema[7.0].define(version: 2026_01_26_143200) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -50,6 +50,15 @@ ActiveRecord::Schema[7.0].define(version: 2026_01_26_133558) do
     t.bigint "blob_id", null: false
     t.string "variation_digest", null: false
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
+  end
+
+  create_table "clubs", force: :cascade do |t|
+    t.string "name"
+    t.string "director_name"
+    t.string "adress"
+    t.string "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "employees", force: :cascade do |t|
