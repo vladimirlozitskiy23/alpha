01009155e8124951f826775be79ee0d49030c0d9
vladimirlozitskiy23 <rubyonrails2023@gmail.com>
2026-01-26 21:08:47 +0300

    Add sport_clubs actions

--- app/controllers/sport_clubs_controller.rb
+++ app/controllers/sport_clubs_controller.rb
@@ -1,2 +1,36 @@
 class SportClubsController < ApplicationController
+
+  def index
+    @sport_club= SportClub.all
+  end
+
+  def new
+    @sport_club = SportClub.new
+  end
+
+  def create 
+    @sport_club = SportClub.new(new_params)
+    if @sport_club=.save 
+      redirect_to @sport_club
+    else
+      render :new
+    end
+  end
+
+  def show
+    @sport_club = SportClub.find(params[:id])
+  end
+
+  def destroy
+    @sport_club = SportClub.find(params[:id])
+    @sport_club.destroy
+    redirect_to @sport_club 
+  end
+
+  private
+
+  def new_params
+    params.require(:sport_club).permit(:name, :director_name, :adress, :email, :phone, :site,  pictures:[])
+  end  
+
 end
