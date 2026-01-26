6baa4bae8aa5c293c99a2c14fc85570200414468
vladimirlozitskiy23 <rubyonrails2023@gmail.com>
2026-01-26 17:53:57 +0300

    Add actions for clubs controller

--- app/controllers/clubs_controller.rb
+++ app/controllers/clubs_controller.rb
@@ -1,2 +1,36 @@
 class ClubsController < ApplicationController
+
+  def index
+    @club = Club.all
+  end
+
+  def new
+    @club = Club.new
+  end
+
+  def create 
+    @club = Club.new(new_params)
+    if @club.save 
+      redirect_to @club
+    else
+      render :new
+    end
+  end
+
+  def show
+    @club = Club.find(params[:id])
+  end
+
+  def destroy
+    @club = Club.find(params[:id])
+    @club.destroy
+    redirect_to @club
+  end
+
+  private
+
+  def new_params
+    params.require(:club).permit(:name, :director_name, :adress, :email,  pictures:[])
+  end  
+
 end
