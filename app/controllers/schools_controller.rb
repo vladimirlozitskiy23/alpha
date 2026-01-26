dbb97044abda356b8330a610a6d1e3ada8ec2137
vladimirlozitskiy23 <rubyonrails2023@gmail.com>
2026-01-26 16:53:22 +0300

    Add actions for schools controller

--- app/controllers/schools_controller.rb
+++ app/controllers/schools_controller.rb
@@ -1,2 +1,36 @@
 class SchoolsController < ApplicationController
+
+  def index
+    @school = School.all
+  end
+
+  def new
+    @school = School.new
+  end
+
+  def create 
+    @school = School.new(new_params)
+    if @school.save 
+      redirect_to @school
+    else
+      render :new
+    end
+  end
+
+  def show
+    @school = School.find(params[:id])
+  end
+
+  def destroy
+    @school = School.find(params[:id])
+    @school.destroy
+    redirect_to @school
+  end
+
+  private
+
+  def new_params
+    params.require(:school).permit(:name, :director_name, :phone, :adress, :email, :site,  pictures:[])
+  end  
+
 end
