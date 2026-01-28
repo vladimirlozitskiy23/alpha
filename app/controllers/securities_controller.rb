7a887573b54bbd260e1bd136d84eb8b02102e578
vladimirlozitskiy23 <rubyonrails2023@gmail.com>
2026-01-28 17:42:40 +0300

    Add securities controller actions

--- app/controllers/securities_controller.rb
+++ app/controllers/securities_controller.rb
@@ -1,2 +1,36 @@
 class SecuritiesController < ApplicationController
+
+  def index
+    @security = security.all
+  end
+
+  def new
+    @security = Security.new
+  end
+
+  def create 
+    @security = Security.new(new_params)
+    if @security.save 
+      redirect_to @security
+    else
+      render :new
+    end
+  end
+
+  def show
+    @security = Security.find(params[:id])
+  end
+
+  def destroy
+    @security = Security.find(params[:id])
+    @security.destroy
+    redirect_to @security
+  end
+
+  private
+
+  def new_params
+    params.require(:security).permit(:name, :director_name, :phone, :adress, :email, :site,  pictures:[])
+  end  
+
 end
