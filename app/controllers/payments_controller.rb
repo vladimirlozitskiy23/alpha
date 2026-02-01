33c09d6d60654038a4820681fd49ef7aad9f9cd5
vladimirlozitskiy23 <rubyonrails2023@gmail.com>
2026-02-01 15:06:27 +0300

    Add payments controller actions

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -1,2 +1,36 @@
 class PaymentsController < ApplicationController
+
+  def index
+    @payment = Payment.all
+  end
+
+  def new
+    @payment = Payment.new
+  end
+
+  def create 
+    @payment = Payment.new(new_params)
+    if @payment.save 
+      redirect_to @payment
+    else
+      render :new
+    end
+  end
+
+  def show
+    @payment = Payment.find(params[:id])
+  end
+
+  def destroy
+    @payment = Payment.find(params[:id])
+    @payment.destroy
+    redirect_to @payment
+  end
+
+  private
+
+  def new_params
+    params.require(:payment).permit(:title, :text,  pictures:[])
+  end  
+
 end
