fc7cfa286bad8369b82f7d3fc9d2deac69d1e53b
vladimirlozitskiy23 <rubyonrails2023@gmail.com>
2026-02-01 18:03:59 +0300

    Create bank_details controller

---
+++ app/controllers/bank_details_controller.rb
@@ -0,0 +1,2 @@
+class BankDetailsController < ApplicationController
+end
---
+++ test/controllers/bank_details_controller_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class BankDetailsControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
