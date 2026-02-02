65d3e3191578a915a19593f57ace570cb5c96482
vladimirlozitskiy23 <rubyonrails2023@gmail.com>
2026-02-02 19:01:58 +0300

    Show payment with bank_detail

--- app/controllers/bank_details_controller.rb
+++ app/controllers/bank_details_controller.rb
@@ -1,2 +1,18 @@
 class BankDetailsController < ApplicationController
+
+def show
+  @payment = Payment.find(params[:payment_id])
+  @bank_detail = @payment.bank_detail
+end
+
+  def create
+    @payment = Payment.find(params[:payment_id])
+    @bank_detail = @payment.create_bank_detail(bank_detail_params)
+    redirect_to payment_bank_detail_path(@payment, @bank_detail)
+  end
+
+  def bank_detail_params
+    params.require(:bank_detail).permit(:name, :adress,  :phone, :settlement_account, :correspondent_account, :BIK, 
+                                        :bank_name, :INN, :KPP, :OKPO, :OGRN, :OKWED, pictures:[] )
+  end
 end
