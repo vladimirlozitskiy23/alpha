62881bb74b732668ef6fd519d4445ef8de9f8815
vladimirlozitskiy23 <rubyonrails2023@gmail.com>
2026-02-03 17:04:43 +0300

    Add before_action and get payment and bank_detail instants

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+
+  before_action :set_payment_bank_detail
+
+def set_payment_bank_detail
+  @payments = Payment.all
+  @payment = @payments.first
+  @bank_detail = @payment.bank_detail 
+end
+
 end
