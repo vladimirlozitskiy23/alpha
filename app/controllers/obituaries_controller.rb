73e8f9a30274dd3f95b3f10462130606f2194bd7
vladimirlozitskiy23 <rubyonrails2023@gmail.com>
2026-02-03 20:11:59 +0300

    Add obituary controller actions

--- app/controllers/obituaries_controller.rb
+++ app/controllers/obituaries_controller.rb
@@ -1,2 +1,36 @@
 class ObituariesController < ApplicationController
+
+  def index
+    @obituaries = Obituary.all
+  end
+
+  def new
+    @obituary = Obituary.new
+  end
+
+  def create 
+    @obituary = Obituary.new(obituary_params)
+    if @obituary.save 
+      redirect_to @obituary
+    else
+      render :new
+    end
+  end
+
+  def show
+    @obituary = Obituary.find(params[:id])
+  end
+
+  def destroy
+    @obituary = Obituary.find(params[:id])
+    @obituary.destroy
+    redirect_to @obituary
+  end
+
+  private
+
+  def obituary_params
+    params.require(:obituary).permit(:name, :rank, :biography, :death,  pictures:[])
+  end  
+
 end
