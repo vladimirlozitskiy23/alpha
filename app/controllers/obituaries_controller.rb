f1908fb0e6ee747b049f4b6835be600e361e1433
vladimirlozitskiy23 <rubyonrails2023@gmail.com>
2026-02-03 20:01:51 +0300

    Add obituaries controller

---
+++ app/controllers/obituaries_controller.rb
@@ -0,0 +1,2 @@
+class ObituariesController < ApplicationController
+end
---
+++ test/controllers/obituaries_controller_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class ObituariesControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
