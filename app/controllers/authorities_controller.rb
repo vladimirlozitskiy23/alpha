031ffe39923ca95f208bfdc49e694201603dee99
vladimirlozitskiy23 <rubyonrails2023@gmail.com>
2026-02-05 16:01:47 +0300

    Create create and show action

--- app/controllers/authorities_controller.rb
+++ app/controllers/authorities_controller.rb
@@ -4,4 +4,22 @@ def new
   @authority = Authority.new
 end
 
+def create 
+  @authority = Authority.new(authority_params)
+  if @authority.save
+    redirect_to @authority
+  else
+    render :new
+  end
+
+end
+
+def show
+  @authority = Authority.find(params[:id])
+end
+
+def authority_params
+  params.require(:authority).permit(:name, :rank, :position, :biography, :status )
+end
+
 end
