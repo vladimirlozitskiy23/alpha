5ad15efc0b306466c2579ed66b22509e57f3b80e
vladimirlozitskiy23 <rubyonrails2023@gmail.com>
2026-02-11 18:07:37 +0300

    Add action new create show to awards controller

--- app/controllers/awards_controller.rb
+++ app/controllers/awards_controller.rb
@@ -1,2 +1,25 @@
 class AwardsController < ApplicationController
+
+def new
+  @award = Award.new  
+end
+
+
+def create
+  @award = Award.new(awards_params)
+  if @award.save
+    redirect_to @award
+  else
+    render :new
+  end
+end
+
+def show 
+  @award = Award.find(params[:id])
+end
+
+def awards_params
+  params.require(:award).permit(:name, :text)
+end
+
 end
